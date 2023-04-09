90045064d4b2e468037b3d8d94c7b3b01519bc74
Raghul Prabhakaran <66682057+RaghulPrabhakaran@users.noreply.github.com>
2023-04-09 14:45:53 +0000

    added bootstrap

--- app.py
+++ app.py
@@ -2,10 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': "Data Analyst",
+  'location': 'Chennai,India',
+  'salary': 'Rs.5,00,000'
+}, {
+  'id': 2,
+  'title': "Data Scientist",
+  'location': 'Banglore,India',
+  'salary': 'Rs.20,00,000'
+}, {
+  'id': 3,
+  'title': "Full-Stack Engineer",
+  'location': 'Remote',
+  'salary': '$4,500'
+}, {
+  'id': 4,
+  'title': "Network Engineer",
+  'location': 'Coimabtore,India',
+  'salary': 'Rs.15,00,000'
+}]
+
 
 @app.route("/")
 def HelloWorld():
-  return render_template('index.html')
+  return render_template('index.html', jobs=JOBS)
 
 
 if __name__ == '__main__':
